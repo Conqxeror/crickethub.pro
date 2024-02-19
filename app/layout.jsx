870395772cc298fbaea49ca6998d5079bac5b5ef
conqxeror <kadriwalimohammad@gmail.com>
2024-02-19 15:39:22 +0530

    added opengraph -meta

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,6 +8,21 @@ export const metadata = {
   description: "Get live scores, stats, player profiles, & analysis on CricketHub.pro. Stay updated on international, domestic, & T20 matches. Join thousands of fans!",
   other: {
     "google-site-verification" : "sVdN8Q1bz1KwTehC2WzKpxZCSttbZ3yjKeD1lTwCIck",
+  },
+  openGraph: {
+    title: 'CricketHub.pro',
+    description: 'Get live scores, stats, player profiles, & analysis on CricketHub.pro. Stay updated on international, domestic, & T20 matches. Join thousands of fans!',
+    url: 'https://crickethub-pro.vercel.app',
+    siteName: 'CricketHub.pro',
+    images: [
+      {
+        url: './opengraph-image.jpg',
+        width: 800,
+        height: 600,
+      }
+    ],
+    locale: 'en_US',
+    type: 'website',
   }
 };
 
